e59735a6fd1e8253add1df98f19ce82e6daa8782
DATA EXPUNGED <trandinhlanntt@gmail.com>
2024-12-24 11:54:52 +0700

    final

---
+++ src/main/java/com/example/t2305m_wcd/entity/PlayerIndex.java
@@ -0,0 +1,16 @@
+package com.example.t2305m_wcd.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PlayerIndex {
+    private Long id;
+    private Long playerId;
+    private Long indexId;
+
+    private Float value;
+}
